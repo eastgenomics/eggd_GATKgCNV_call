94e75b6bb7e6cbcc113bb9faa1665536e2aba047
sophie22 <rzsozso@gmail.com>
2022-05-13 11:14:10 +0100

    formatting, docs

--- src/GATK_gCNVcaller.sh
+++ src/GATK_gCNVcaller.sh
@@ -23,14 +23,17 @@ main() {
     docker load -i GATK.tar.gz
 
     # Parse the image ID from the list of docker images
+    # need to export variables so they're available to parallel
     export GATK_image=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^broad" | cut -d' ' -f2)
     export $CollectReadCounts_args
     export $PostprocessGermlineCNVCalls_args
 
     ## Create folder to collect input files:
     mkdir inputs
+
     # Prior probabilities tsv
-    if [[ ! -z $prior_prob ]]; then
+    if [[ ! -z $prior_prob ]]
+    then
         echo "Prior prob file is provided as '$prior_prob'"
         dx download "$prior_prob" -o inputs/prior_prob.tsv
     else
@@ -39,7 +42,7 @@ main() {
 
     cd inputs
     mkdir beds
-    mark-section "Downloading input bed files"
+    mark-section "Downloading input files"
     # Intervals file (preprocessed bed from GATK_prep)
     dx download "$interval_list" -o beds/preprocessed.interval_list
     # Annotation tsv (from GATK_prep)
@@ -53,33 +56,27 @@ main() {
     do
         dx download "${bambis[$i]}"
     done
-    cd ..
-    echo "All input files have downloaded to inputs/"
-    cd ..
 
+    cd /home/dnanexus
+    echo "All input files have downloaded to inputs/"
+
+    # Optional to hold job after downloading all input files
     if $debug_fail_start; then exit 1; fi
 
     # 1. Run CollectReadCounts:
     # takes a bam file at a time (and its index file) and the target.bed
     echo "Running CollectReadCounts for all input bams"
     mark-section "CollectReadCounts"
-        # mkdir inputs/base_counts
-        # for bam_file in inputs/bams/*.bam
-        # do
-        #     sample_file=$( basename $bam_file )
-        #     sample_name="${sample_file%.bam}"
-        #     echo $sample_name
-
-        #     time docker run -v /home/dnanexus/inputs:/data broadinstitute/gatk:4.1.2.0 gatk CollectReadCounts \
-        #         -I /data/bams/${sample_file} \
-        #         -L /data/beds/preprocessed.interval_list -imr OVERLAPPING_ONLY \
-        #         --format TSV \
-        #         -O /data/base_counts/${sample_name}_basecount.tsv
-        #     # outputs a single tsv file per sample
-        # done
-
     mkdir inputs/base_counts
-    find inputs/bams/ -name "*.bam" | parallel -I filename --max-args 1 --jobs 8 'sample_file=$( basename filename ); sample_name="${sample_file%.bam}"; echo $sample_name; /usr/bin/time -v sudo docker run -v /home/dnanexus/inputs:/data $GATK_image gatk CollectReadCounts -I /data/bams/${sample_file} -L /data/beds/preprocessed.interval_list -imr OVERLAPPING_ONLY ${CollectReadCounts_args} -O /data/base_counts/${sample_name}_basecount.hdf5'
+    find inputs/bams/ -name "*.bam" | parallel -I filename --max-args 1 --jobs 8 \
+    'sample_file=$( basename filename ); \
+    sample_name="${sample_file%.bam}"; \
+    echo $sample_name; \
+    /usr/bin/time -v sudo docker run -v /home/dnanexus/inputs:/data $GATK_image gatk CollectReadCounts \
+    -I /data/bams/${sample_file} \
+    -L /data/beds/preprocessed.interval_list -imr OVERLAPPING_ONLY \
+    ${CollectReadCounts_args} \
+    -O /data/base_counts/${sample_name}_basecount.hdf5'
 
     # prepare a batch_input string that has all sample_basecount.tsv file as an input
     batch_input=""
@@ -100,75 +97,65 @@ main() {
     echo "Identifying excluded intervals from CNV calling on this run"
     bedtools intersect -v -a <(tail +88 inputs/beds/preprocessed.interval_list | sort) \
         -b <(tail +88 inputs/beds/filtered.interval_list | sort) > excluded_intervals.bed
-    # wc -l excluded_intervals.bed
 
     # 2. Run DetermineGermlineContigPloidy:
     # takes the base count tsv-s from the previous step, optional target_bed, and a contig plody priors tsv
+    # outputs a ploidy model and ploidy-calls for each sample
     echo "Running DetermineGermlineContigPloidy for the calculated basecounts"
     mark-section "DetermineGermlineContigPloidy"
     mkdir inputs/ploidy-dir
     /usr/bin/time -v docker run -v /home/dnanexus/inputs:/data $GATK_image gatk DetermineGermlineContigPloidy \
         -L /data/beds/filtered.interval_list -imr OVERLAPPING_ONLY \
+        $DetermineGermlineContigPloidy_args \
         $batch_input \
-        --contig-ploidy-priors /data/prior_prob.tsv --output-prefix ploidy \
+        --contig-ploidy-priors /data/prior_prob.tsv \
+        --output-prefix ploidy \
         -O /data/ploidy-dir
-    # outputs a ploidy model and ploidy-calls for each sample
 
     # 3. Run GermlineCNVCaller:
     # takes the base count tsv-s, target bed and contig ploidy calls from the previous steps
+    # outputs a CNVcalling model and copy ratio files for each sample
     echo "Running GermlineCNVCaller for the calculated basecounts using the generated ploidy file"
     mark-section "GermlineCNVCaller"
     mkdir inputs/gCNV-dir
     /usr/bin/time -v docker run -v /home/dnanexus/inputs:/data $GATK_image gatk GermlineCNVCaller \
-        $batch_input --run-mode COHORT  $GermlineCNVCaller_args \
         -L /data/beds/filtered.interval_list -imr OVERLAPPING_ONLY \
         --annotated-intervals /data/beds/annotated_intervals.tsv \
-        --contig-ploidy-calls /data/ploidy-dir/ploidy-calls/ --output-prefix CNV \
+        --run-mode COHORT \
+        $GermlineCNVCaller_args \
+        $batch_input \
+        --contig-ploidy-calls /data/ploidy-dir/ploidy-calls/ \
+        --output-prefix CNV \
         -O /data/gCNV-dir
-    # outputs a CNVcalling model and copy ratio files for each sample
 
     # 4. Run PostprocessGermlineCNVCalls:
     # takes CNV-model in, spits vcfs out
     echo "Running PostprocessGermlineCNVCalls"
-        # i=0
-        # for bam_file in inputs/bams/*.bam
-        # do
-        #     sample_file=$( basename $bam_file )
-        #     sample_name="${sample_file%.bam}"
-        #     echo $sample_name
-
-        #     docker run -v /home/dnanexus/inputs:/data broadinstitute/gatk:4.1.2.0 gatk PostprocessGermlineCNVCalls \
-        #     --contig-ploidy-calls /data/ploidy-dir/ploidy-calls \
-        #     --calls-shard-path /data/gCNV-dir/CNV-calls/ \
-        #     --model-shard-path /data/gCNV-dir/CNV-model/ \
-        #     --sample-index $i \
-        #     --autosomal-ref-copy-number 2 \
-        #     --allosomal-contig X \
-        #     --allosomal-contig Y \
-        #     --output-genotyped-intervals /data/vcfs/${sample_name}_intervals.vcf \
-        #     --output-genotyped-segments /data/vcfs/${sample_name}_segments.vcf
-        
-        #     ((i=i+1))
-        # done
-
+    mark-section "PostprocessGermlineCNVCalls"
         # Required Arguments for 4.2.5.0: (4.2 onwards)
-        ## --calls-shard-path <File>     List of paths to GermlineCNVCaller call directories.  This argument must be specified atleast once. Required. 
-        ## --contig-ploidy-calls <File>  Path to contig-ploidy calls directory (output of DetermineGermlineContigPloidy). Required. 
-        ## --model-shard-path <File>     List of paths to GermlineCNVCaller model directories.  This argument must be specified atleast once. Required. 
+        # --calls-shard-path <File>     List of paths to GermlineCNVCaller call directories.  This argument must be specified atleast once. Required. 
+        # --contig-ploidy-calls <File>  Path to contig-ploidy calls directory (output of DetermineGermlineContigPloidy). Required. 
+        # --model-shard-path <File>     List of paths to GermlineCNVCaller model directories.  This argument must be specified atleast once. Required. 
         # --output-denoised-copy-ratios <File> Output denoised copy ratio file.  Required. 
         # --output-genotyped-intervals <File>  Output intervals VCF file.  Required. 
         # --output-genotyped-segments <File> Output segments VCF file.  Required. 
 
-    mark-section "PostprocessGermlineCNVCalls"
     mkdir inputs/vcfs
+    # command finds sample data based on an arbitrary index which needs to be passed to parallel
+    # index is created based on the number of input bams
+    # triple colon at the end is the parallel way to provide an array of integers
     sample_num=$(ls inputs/bams/*.bam | wc -l)
     index=$(expr $sample_num - 1)
     parallel --jobs 8 '/usr/bin/time -v docker run -v /home/dnanexus/inputs:/data $GATK_image \
-        gatk PostprocessGermlineCNVCalls --sample-index {} \
+        gatk PostprocessGermlineCNVCalls \
+        --sample-index {} \
         ${PostprocessGermlineCNVCalls_args} \
-        --autosomal-ref-copy-number 2 --allosomal-contig X --allosomal-contig Y \
+        --autosomal-ref-copy-number 2 \
+        --allosomal-contig X \
+        --allosomal-contig Y \
         --contig-ploidy-calls /data/ploidy-dir/ploidy-calls \
-        --calls-shard-path /data/gCNV-dir/CNV-calls --model-shard-path /data/gCNV-dir/CNV-model \
+        --calls-shard-path /data/gCNV-dir/CNV-calls \
+        --model-shard-path /data/gCNV-dir/CNV-model \
         --output-genotyped-intervals /data/vcfs/sample_{}_intervals.vcf \
         --output-genotyped-segments /data/vcfs/sample_{}_segments.vcf \
         --output-denoised-copy-ratios /data/vcfs/sample_{}_denoised_copy_ratios.tsv \
@@ -184,19 +171,10 @@ main() {
     '
 
     echo "CNV calling finished successfully"
-    ## Create output directories
-    # and move result files into outdir to be uploaded
-    vcf_dir=out/result_files/CNV_vcfs && mkdir -p ${vcf_dir}
-    cp inputs/vcfs/*.vcf ${vcf_dir}/
-    # copy_ratio_dir=out/result_files/CNV_copy_ratios && mkdir -p ${copy_ratio_dir}
-    # cp inputs/vcfs/*_denoised_copy_ratios.tsv ${copy_ratio_dir}
-    summary_dir=out/result_files/CNV_summary && mkdir -p ${summary_dir}
-    cp excluded_intervals.bed ${summary_dir}/$run_name"_excluded_intervals.bed"
 
-    # Move result files into outdir to be uploaded
-    # outdir=out/result_files && mkdir -p ${outdir}
-    # mv inputs/ploidy-dir/* ${outdir}/
-    # cp -r inputs/gCNV-dir/CNV-calls ${outdir}/
+    ## Create output directories
+    vcf_dir=out/result_files/CNV_vcfs && mkdir -p ${vcf_dir}
+    summary_dir=out/result_files/CNV_summary && mkdir -p ${summary_dir}
 
     if $toAnnotate; then
         mark-section "Annotating calls"
@@ -231,6 +209,10 @@ main() {
 
     echo "All scripts finished successfully, uploading output files to dx"
     if $debug_fail_end; then exit 1; fi
+
+    # and move result files into outdir to be uploaded
+    mv inputs/vcfs/*.vcf ${vcf_dir}/
+    mv excluded_intervals.bed ${summary_dir}/$run_name"_excluded_intervals.bed"
 
     # Upload output files
     dx-upload-all-outputs --parallel
