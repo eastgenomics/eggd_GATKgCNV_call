9706a67217b494a1bf16207f1a2b36583591b8e8
ChrisPyatt <christopher.pyatt1@nhs.net>
2024-06-14 22:27:18 +0100

    cracked basecount access (I think)

--- src/GATK_gCNVcaller.sh
+++ src/GATK_gCNVcaller.sh
@@ -50,13 +50,9 @@ main() {
     dx download "$annotation_tsv" -o beds/annotated_intervals.tsv
 
     mkdir bams
-    cd bams
     ## Download all input bam and bai files
     mark-section "Downloading input bam & bai files"
-    for i in ${!bambais[@]}
-    do
-        dx download "${bambais[$i]}"
-    done
+    echo ${bambais[@]} | jq -r '.["$dnanexus_link"]' | xargs -n1 -P$(nproc --all) dx download --no-progress -o bams/
 
     cd /home/dnanexus
     echo "All input files have downloaded to inputs/"
@@ -80,10 +76,14 @@ main() {
     -O /data/base_counts/${sample_name}_basecount.hdf5'
 
     # prepare a batch_input string that has all sample_basecount.tsv file as an input
+    # and another that has the fileIDs for cnv_call subjobs
     batch_input=""
+    #basecount_ids=""
     for base_count in inputs/base_counts/*_basecount.hdf5; do
         sample_file=$( basename $base_count )
         batch_input+="--input /data/base_counts/${sample_file} "
+        #basecount_id=$( dx upload --brief $base_count )
+        #basecount_ids+="-ibasecounts=$basecount_id "
     done
 
     # 2. Run FilterIntervals:
@@ -121,6 +121,16 @@ main() {
         --contig-ploidy-priors /data/prior_prob.tsv \
         --output-prefix ploidy \
         -O /data/ploidy-dir
+    
+    # Make batch list of ploidy fileIDs for cnv_call subjobs
+    #ploidycall_ids=""
+    #for ploidy_call in inputs/ploidy-dir/*; do
+    #    ploidycall_id=$( dx upload --brief $ploidy_call )
+    #    ploidycall_ids+="-iploidycalls=$ploidycall_id "
+    #done
+
+    dx upload -rp /home/dnanexus/inputs/ploidy-dir
+    dx upload -rp /home/dnanexus/inputs/base_counts
 
     # 4. Run GermlineCNVCaller:
     # takes the base count tsv-s, target bed and contig ploidy calls from the previous steps
@@ -128,15 +138,35 @@ main() {
     echo "Running GermlineCNVCaller for the calculated basecounts using the generated ploidy file"
     mark-section "GermlineCNVCaller"
     mkdir inputs/gCNV-dir
-    /usr/bin/time -v docker run -v /home/dnanexus/inputs:/data $GATK_image gatk GermlineCNVCaller \
-        -L /data/beds/filtered.interval_list -imr OVERLAPPING_ONLY \
-        --annotated-intervals /data/beds/annotated_intervals.tsv \
-        --run-mode COHORT \
-        $GermlineCNVCaller_args \
-        $batch_input \
-        --contig-ploidy-calls /data/ploidy-dir/ploidy-calls/ \
-        --output-prefix CNV \
-        -O /data/gCNV-dir
+    tsv=/home/dnanexus/inputs/beds/annotated_intervals.tsv
+    ints=/home/dnanexus/inputs/beds/filtered.interval_list
+    if [ $split_by_chromosome == 'true' ]; then
+        # make new intervals files (split by chromosome) & initiate cnv calling in subjobs
+        chroms=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y )
+        cnv_call_jobs=()
+        for i in ${chroms[@]}; do
+            echo "Chromosome $i"
+            chr_tsv=/home/dnanexus/inputs/beds/chr"$i"_annotation.tsv
+            chr_ints=/home/dnanexus/inputs/beds/chr"$i".interval_list
+            grep ^@ $tsv > $chr_tsv; grep ^CONTIG $tsv >> $chr_tsv; grep -P "^$i\t" $tsv >> $chr_tsv
+            grep ^@ $ints > $chr_ints; grep -P "^$i\t" $ints >> $chr_ints
+            chr_tsv=$( dx upload --brief $chr_tsv )
+            chr_ints=$( dx upload --brief $chr_ints )
+            job_name='"$i"_cnv_call'
+            command="dx-jobutil-new-job call_cnvs \
+                -iannotation_tsv='$chr_tsv' -iinterval_list='$chr_ints' \
+                -iGATK_docker='$GATK_docker' \
+                -iGermlineCNVCaller_args='$GermlineCNVCaller_args' \
+                --name $job_name"
+            cnv_call_jobs+=($(eval $command))
+        done
+    else
+        command="dx-jobutil-new-job call_cnvs -iannotation_tsv=$tsv -iinterval_list=$ints"
+        cnv_call_jobs+=($(eval $command))
+    fi
+
+    # Wait for all subjobs to finish before grabbing outputs
+    dx wait "${cnv_call_jobs[@]}"
 
     # 5. Run PostprocessGermlineCNVCalls:
     # takes CNV-model in, spits vcfs out
@@ -208,4 +238,40 @@ main() {
     # Upload output files
     dx-upload-all-outputs --parallel
 
+}
+
+call_cnvs() {
+    dx-download-all-inputs
+
+    mkdir -p /home/dnanexus/in/basecounts
+    dx download $( dx find data --project $DX_WORKSPACE_ID --name *hdf5 --brief )
+    mv *hdf5 /home/dnanexus/in/basecounts/
+
+    # Make basecount batch string
+    batch_input=""
+    for base_count in /home/dnanexus/in/basecounts/*_basecount.hdf5; do
+        sample_file=$( basename $base_count )
+        batch_input+="--input /data/basecounts/${sample_file} "
+    done
+
+    # Load the GATK docker image
+    mark-section "Loading GATK Docker image"
+    docker load -i /home/dnanexus/in/GATK_docker/GATK*.tar.gz
+
+    # Declare ENV variable for command
+    export GATK_image=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^broad" | cut -d' ' -f2)
+
+    # Run CNV caller
+    /usr/bin/time -v docker run -v /home/dnanexus/in/:/data/ $GATK_image gatk GermlineCNVCaller \
+        -L /data/interval_list/*.interval_list -imr OVERLAPPING_ONLY \
+        --annotated-intervals /data/annotation_tsv/*_annotation.tsv \
+        --run-mode COHORT \
+        $GermlineCNVCaller_args \
+        $batch_input \
+        --contig-ploidy-calls /home/dnanexus/in/ploidy-dir/ploidy-calls/ \
+        --output-prefix CNV \
+        -O /data/gCNV-dir
+    
+    # Upload outputs back to parent
+    dx-upload-all-outputs
 }
