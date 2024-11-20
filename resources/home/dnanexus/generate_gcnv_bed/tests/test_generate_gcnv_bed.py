06207a46863a56055cf25e3b4e1ea33b2cfef7fe
Jethro Rainford <rainford1995@gmail.com>
2024-11-20 14:53:40 +0000

    add unit tests for read_all_copy_ratio_files()

--- resources/home/dnanexus/generate_gcnv_bed/tests/test_generate_gcnv_bed.py
+++ resources/home/dnanexus/generate_gcnv_bed/tests/test_generate_gcnv_bed.py
@@ -1,5 +1,11 @@
+"""
+Tests for generate_gcnv_bed.py
+"""
+
+from glob import glob
 import os
 from pathlib import Path
+import shutil
 from uuid import uuid4
 import unittest
 from unittest.mock import patch
@@ -104,3 +110,83 @@ class TestReadSingleCopyRatioFile(unittest.TestCase):
             read_single_copy_ratio_file(
                 copy_ratio_file=self.sample_1_test_file
             )
+
+
+class TestReadAllCopyRatioFiles(unittest.TestCase):
+    def test_reading_all_files_to_dataframe_correct(self):
+        copy_ratio_files = glob(f"{TEST_DATA_DIR}/*copy_ratios.tsv")
+
+        copy_ratio_df = read_all_copy_ratio_files(
+            copy_ratio_files=copy_ratio_files
+        )
+
+        expected_df = pd.DataFrame(
+            {
+                "chr": ["1", "1", "1", "1", "1"],
+                "start": [7917094, 10566156, 17345176, 17345330, 17348949],
+                "end": [7917213, 10566275, 17345329, 17345483, 17349114],
+                "sample_1": [
+                    1.9486853760805845,
+                    2.0797460403307504,
+                    1.9807613126697792,
+                    2.0011478190013547,
+                    1.9811431848626703,
+                ],
+                "sample_2": [
+                    1.6825420819612993,
+                    1.8989351264973224,
+                    1.949430403226796,
+                    1.93894585147577,
+                    2.015844320949207,
+                ],
+                "sample_3": [
+                    2.0369879930267016,
+                    2.0132574495419986,
+                    1.9679869323497048,
+                    2.139656851102916,
+                    1.9622012727554363,
+                ],
+            },
+        )
+
+        self.assertTrue(copy_ratio_df.equals(expected_df))
+
+    def test_value_error_raised_when_file_with_different_intervals_parsed(
+        self,
+    ):
+        """
+        Test we correctly raise a ValueError when not all files have the
+        same intervals by making an additional test copy ratio file
+        and dropping some rows
+        """
+        diff_intervals_tsv = (
+            Path(__file__)
+            .absolute()
+            .parent.joinpath("diff_intervals_copy_ratios.tsv")
+        )
+
+        with open(
+            Path(TEST_DATA_DIR).joinpath("sample_1_denoised_copy_ratios.tsv"),
+            encoding="utf-8",
+            mode="r",
+        ) as fh:
+            sample_1_contents = fh.read().splitlines()
+
+        with open(
+            diff_intervals_tsv,
+            encoding="utf-8",
+            mode="w",
+        ) as fh:
+            fh.write("\n".join(sample_1_contents[:-2]))
+
+        copy_ratio_files = glob(f"{TEST_DATA_DIR}/*copy_ratios.tsv")
+        copy_ratio_files.append(diff_intervals_tsv)
+
+        with self.subTest():
+            with pytest.raises(
+                ValueError,
+                match="Copy ratio file for sample_1",
+            ):
+                read_all_copy_ratio_files(copy_ratio_files=copy_ratio_files)
+
+        os.remove(diff_intervals_tsv)
