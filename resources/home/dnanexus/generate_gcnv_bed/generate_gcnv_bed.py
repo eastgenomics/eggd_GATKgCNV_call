b12e1826cbd1a30b861d885b6c43f7195760934d
ChrisPyatt <christopher.pyatt1@nhs.net>
2025-01-16 14:33:52 +0000

    whitespace and actions

--- resources/home/dnanexus/generate_gcnv_bed/generate_gcnv_bed.py
+++ resources/home/dnanexus/generate_gcnv_bed/generate_gcnv_bed.py
@@ -168,7 +168,7 @@ def read_all_copy_ratio_files(copy_ratio_files) -> pd.DataFrame:
     # Start coordinates are currently 1-based, need to be offset by 1
     # to create a 0-based BED file for IGV visualisation
     copy_ratio_df['start'] = copy_ratio_df['start'].apply(lambda x: x-1)
-    
+
     print(
         f"Completed reading {len(copy_ratio_files)} copy ratio "
         f"files in {round(timer() - start, 2)}s"
